2aab40ea5e2fad4ee9414ebacfdeeb6ed6dd7e52
limms-pm24-wq <limms-pm24@student.tarc.edu.my>
2025-12-06 14:58:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cpcs6poize7u.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee_db"
+custombucket = "miaosoon-bucket"
+customregion = "us-east-1"
+
